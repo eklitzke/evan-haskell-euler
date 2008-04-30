684de1b249b5b354b9be1df78a822fe406a0e98b
Evan Klitzke <evan@evan-mbp-ubuntu.(none)>
2008-04-29 22:52:08 -0700

    tiny refactor to solution for 57

--- 57/EulerProblem.hs
+++ 57/EulerProblem.hs
@@ -8,13 +8,13 @@ fracRecip (Frac n d) = Frac d n
 fracInc :: Fraction -> Fraction
 fracInc (Frac n d) = Frac (n + d) d
 
+nextFrac :: Fraction -> Fraction
+nextFrac = fracInc . fracRecip . fracInc
+
+fracs :: [Fraction]
+fracs = ((Frac 3 2) : [nextFrac f | f <- fracs])
+
 magic :: Fraction -> Bool
 magic (Frac n d) = (length $ show n) > (length $ show d)
 
-nextFrac :: Fraction -> Fraction
-nextFrac f = fracInc (fracRecip (fracInc f))
-
-fracs :: [Fraction]
-fracs = take 1000 ((Frac 3 2) : [nextFrac f | f <- fracs])
-
-main = print $ length $ filter magic fracs
+main = print $ length $ filter magic $ take 1000 fracs

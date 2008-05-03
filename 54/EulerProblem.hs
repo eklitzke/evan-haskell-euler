4d57fc0ed5e717f240f7dbe2c2d11ca2cafb6e9e
Evan Klitzke <evan@evan-mbp-ubuntu.(none)>
2008-05-02 20:44:06 -0700

    some progress on #54

--- 54/EulerProblem.hs
+++ 54/EulerProblem.hs
@@ -2,14 +2,17 @@ module Main where
 
 import Foreign
 import Data.Char
+import Data.List
 
 -- Define some card types so that we can compare cards naturally
 
+data Game = Win | Loss | Tie
 data Suit = Hearts | Clubs | Diamonds | Spades deriving (Eq, Ord)
 data Card = Card Int Suit deriving (Eq)
 type Hand = [Card]
-
-type HandRank = HighCard | OnePair | TwoPair | TheeKind | Straight | Flush | FullHouse | FourKind | StraightFlush | RoyalFlush deriving (Eq, Ord)
+type Set  = [Card] -- This is something like a flush or a full house
+type HandSet = (Hand, Set)
+data HandRank = HighCard | OnePair | TwoPair | ThreeKind | Straight | Flush | FullHouse | FourKind | StraightFlush | RoyalFlush deriving (Eq, Ord)
 
 instance Ord Card where
     compare (Card a b) (Card c d)
@@ -43,13 +46,62 @@ lineToHands l = (mcr h1, mcr h2)
     h2 = drop 5 lineWords
     mcr = map cardRead
 
-pokerHands :: [(Hand, Hand)]
+getSuit :: Card -> Suit
+getSuit (Card _ s) = s
+
+getCardInt :: Card -> Int
+getCardInt (Card i _) = i
+
+-- tests whether a hand is a flush
+isFlush :: Hand -> Bool
+isFlush (c:cs) = all (== (getSuit c)) (map getSuit cs)
+
+isStraight :: Hand -> Bool
+isStraight h = isStraight' h
+    where
+    isStraight' []  = True
+    isStraight' [x] = True
+    isStraight' (c:c':cs) = (c == (c' - 1)) && (isStraight' (c':cs))
+
+allSameInList :: Eq a => [a] -> Bool
+allSameInList [] = True
+allSameInList (c:cs) = all (== c) cs
+
+
+countSortedList :: Ord a => [a] -> [(a, Int)]
+countSortedList []     = []
+countSortedList (x:xs) = (x, ct) : (countSortedList rm)
+    where
+    consume x n (y:ys) = if y == x
+                            then consume x (n+1) ys 
+                            else (n, y:ys)
+    (ct, rm) = consume x 1 xs
+
+countList :: Ord a => [a] -> [(a, Int)]
+countList = countSortedList . sort
+
+getHandRank :: Hand -> HandRank
+getHandRank h
+    | (isFlush h) && ((sort $ map getCardInt h) == [1, 10, 11, 12, 13])
+      = RoyalFlush
+    | (isFlush h) && (isStraight h) 
+      = StraightFlush
+    | (allSameInList $ init svals) || (allSameInList $ tail svals) 
+      = FourKind
+    | (length $ nub $ vals) == 2
+      = FullHouse
+    | isFlush h
+      = Flush
+    | isStraight h
+      = Straight
+    where
+    vals = map getCardInt h
+    svals = sort vals
+
+pokerHands :: [(HandSet, HandSet)]
 pokerHands = map lineToHands (lines pokerTxt)
     where
     pokerTxt = unsafePerformIO (readFile "poker.txt")
-
-tieBreaker :: Hand -> Hand -> Bool
-tieBreaker 
 
 a :: Card
 a = cardRead "4H"

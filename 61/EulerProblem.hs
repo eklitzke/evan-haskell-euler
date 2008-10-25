4409ce65ff9f099b483e27670931fa0808d74635
Evan Klitzke <evan@eklitzke.org>
2008-10-25 16:30:38 -0700

    sigh

--- 61/EulerProblem.hs
+++ 61/EulerProblem.hs
@@ -1,7 +1,7 @@
 module Main where
 
-import Data.Set as S
-import Data.List as L
+import Data.Set (Set, fromDistinctAscList, member)
+import Data.List
 
 fours :: [Int]
 fours = [1000 .. 9999]
@@ -13,114 +13,47 @@ makePolygonalSet f = fromDistinctAscList $ fourDigits [f n | n <- [1..]]
 triangles, squares, pentagonals, hexagonals, heptagonals, octagonals :: Set Int
 triangles   = makePolygonalSet (\n -> (n*(n+1)) `div` 2)
 squares     = makePolygonalSet (\n -> n*n)
-pentagonals = makePolygonalSet (\n -> (n*(3*n-1)) `div` 2)
-hexagonals  = makePolygonalSet (\n -> n * (2*n - 1))
-heptagonals = makePolygonalSet (\n -> (n*(5*n-3)) `div` 2)
+pentagonals = makePolygonalSet (\n -> (n*(3*n - 1)) `div` 2)
+hexagonals  = makePolygonalSet (\n -> n * (2 * n - 1))
+heptagonals = makePolygonalSet (\n -> (n*(5*n - 3)) `div` 2)
 octagonals  = makePolygonalSet (\n -> n * (3*n - 2))
 
-firstAndLast xs = (take 2 $ head xs) == (drop 2 $ last xs)
+-- cryptic
+secret :: [a] -> [[a]]
+secret []     = []
+secret (x:xs) = (x:xs) : [y:x:ys | y:ys <- secret xs]
 
-cyclic (x:y:xs) = ((drop 2 x) == (take 2 y)) && (cyclic (y:xs))
-cyclic _        = True
-
--- Tests if a list of numbers is cyclic, as defined in the problem
-hasCyclicProperty :: [Int] -> Bool
-hasCyclicProperty = hasCyclicProperty' . L.map show
-    where
-    hasCyclicProperty' :: [String] -> Bool
-    hasCyclicProperty' xs = (cyclic xs) && (firstAndLast xs)
+magic :: [Int] -> [Set Int] -> Bool
+magic [] _ = True
+magic ns (s:ss) = any magic' (secret ns)
+    where magic' (x:xs) = (x `member` s) && (magic xs ss)
 
 hasPolygonalProperty :: [Int] -> Bool
-hasPolygonalProperty [a,b,c,d,e,f] = and [ a `S.member` triangles
-                                         , b `S.member` squares
-                                         , c `S.member` pentagonals
-                                         , d `S.member` hexagonals
-                                         , e `S.member` heptagonals
-                                         , f `S.member` octagonals ]
+hasPolygonalProperty xs = magic xs [triangles, squares, pentagonals, hexagonals, heptagonals, octagonals]
+--hasPolygonalProperty xs = magic xs [triangles, squares, pentagonals]
 
 -- Allowable two digit sequences
 allowable :: [Int]
-allowable = [x | x <- [11..99], (x `mod` 10) /= 0]
+allowable = [10..99]
 
-generateNextCyclic :: Int -> [Int]
-generateNextCyclic x =
-    let lastTwo = (read (drop 2 $ show x)) * 100
-     in [lastTwo + x | x <- allowable]
+nextCyclics :: Int -> [Int]
+nextCyclics x = let x' = (x `mod` 100) * 100 in [x' + y | y <- allowable]
 
+-- any cycle will have repeats generated, because the cycle can be rotated and
+-- will also be a cycle. We want to avoid reprocessing these rotated cycles.
+-- Therefore, we adopt that the canonical form of a cycle is the form that has
+-- the smallest element as the first element.
+isCanonical :: [Int] -> Bool
+isCanonical (x:xs) = x < minimum xs
 
-possibleStarts :: [Int] 
-possibleStarts = [a * 100 + b | a <- allowable | b <- allowable]
-
-generateCyclics :: Int -> [[Int]]
-generateCyclics n = generate' 5 n
+generateCycles :: Int -> [[Int]]
+generateCycles num = filter isCanonical (generate' 6 num)
     where
-    generate' 0 _ = []
-    generate' n x = map ((++) (map (n:) (generateNextCyclic n))
+    generate' 2 x = [[x,z] | z <- nextCyclics x, z `mod` 100 == t]
+    generate' n x = concat [[x:y | y <- generate' (n-1) z] | z <- nextCyclics x]
+    t = num `div` 100
 
-candidates = [[
+allCycles :: [[Int]]
+allCycles = concatMap generateCycles [x * 100 + y | x <- [10..99], y <- [x..99]]
 
-map read [
-    let beg = read $ take 2 $ show x
-     in
-         
-    
-    | (read $ take 2 $ show x) < 10 = []
-    | otherwise                     = 
-generateNextCyclic x =
-    where
-
-
-hasBoth f g = \x -> (f x) && (g x)
-
-myList = [[a,b,c,d,e,f] | a <- [1000..9999], b <- [a..9999], c <- [b..9999], d <- [c..9999], e <- [d..9999], f <- [e..9999]]
-
-main = print $ L.filter (hasBoth hasPolygonalProperty hasCyclicProperty) myList
-{-
-
-allSets = triangles `S.union` squares `S.union` pentagonals `S.union` hexagonals `S.union` heptagonals `S.union` octagonals
-
-makeNPerms :: Int -> [a] -> [[a]]
-makeNPerms 0 _  = [[]]
-makeNPerms n xs = [x:y | x <- xs, y <- makeNPerms (n-1) (tail xs)]
-
--- means there is an ordering of the list that is cylic
---cyclicLists :: [a] -> [[a]]
---cyclicLists = filter hasCyclicProperty $ makeNPerms 6 allSets
-
-prefixes :: Set String
-prefixes = S.map (take 2 . show) allSets
-
-postfixes :: Set String
-postfixes = S.map (drop 2 . show) allSets
-
-pruneSet :: Set Int -> Set Int
-pruneSet = S.intersection pruned
-    where
-    pruned = S.filter (\x -> let sx = show x in ((take 2 sx) `member` prefixes) && (drop 2 sx) `member` postfixes) allSets
-
-pt = pruneSet triangles
-ps = pruneSet squares
-pp = pruneSet pentagonals
-px = pruneSet hexagonals
-ph = pruneSet heptagonals
-po = pruneSet octagonals
-
-ptl = toAscList pt
-psl = toAscList ps
-ppl = toAscList pp
-pxl = toAscList px
-phl = toAscList ph
-pol = toAscList po
-
-main = print $ product [S.size s | s <- [pt, ps, pp, px, ph, po]]
-
---main = print $ (length $ nub [take 2 $ show x| x <- allSets], length $ nub [drop 2 $ show x| x <- allSets])
-{- main = do
-    print $ S.size pt
-    print $ S.size ps
-    print $ S.size pp
-    print $ S.size px
-    print $ S.size ph
-    print $ S.size po
-    -}
-    -}
+main = print $ head $ filter hasPolygonalProperty allCycles
